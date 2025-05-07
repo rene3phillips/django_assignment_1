6d3e6fc101c34b4d6a01a3feb4def4e93f652d2e
rene3phillips <rene3phillips@gmail.com>
2025-05-06 20:10:46 -0400

    Initial commit

---
+++ hms_project/hms_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Doctor, Patient, Appointment, Billing
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(Billing)
